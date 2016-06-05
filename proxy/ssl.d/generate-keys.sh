a2238f60175059ee57dfcbc42f1c75e8ca4acad5
Milan Felix Sulc <rkfelix@gmail.com>
2016-06-05 02:25:14 +0200

    Typo

--- proxy/ssl.d/generate-keys.sh
+++ proxy/ssl.d/generate-keys.sh
@@ -3,5 +3,5 @@
 # Generate self-signed key + cert
 openssl req -new -x509 -nodes -days 36500 -keyout privkey.pem -out fullchain.pem -config ./openssl.conf
 
-# Haproxy certificate
+# Haproxy/Pound certificate concat
 cat fullchain.pem privkey.pem > cert.pem
